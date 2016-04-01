9014f4a4a7c6bb77a64c4f3b1755c2682f0e94d5
Sergii Bondarenko <broken@propeople.com.ua>
2016-04-01 14:41:03 +0300

    Add initial test case for PHPUnit

---
+++ tests/EnvironmentLoaderTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Sergii Bondarenko, <sb@firstvector.org>
+ */
+namespace Behat\Tests;
+
+/**
+ * Class EnvironmentLoaderTest.
+ *
+ * @package Behat\Tests
+ */
+class EnvironmentLoaderTest extends \PHPUnit_Framework_TestCase
+{
+
+}
